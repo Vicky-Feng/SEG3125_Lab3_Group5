1f4e48b4244d2760f96f97156a573dabc170ae51
Vicky-Feng <71288189+Vicky-Feng@users.noreply.github.com>
2023-01-30 16:36:37 -0500

    update navigate bar design

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,14 @@
 
 // This function is called when any of the tab is clicked
 // It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+}
 
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
 function openInfo(evt, tabName) {
 
 	// Get all elements with class="tabcontent" and hide them
